5b4da7143f02f7f6f7099857e139afb3794f47e6
Daniel <clancy.daniel@gmail.com>
2018-02-23 14:25:36 -0600

    root route shows users now

--- server.py
+++ server.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 app.secret_key = 'jkfu890342htruo34v7yut8039pthjiopv78t0432-y5t3480wtb342y905n34um20w'
 
 from mysqlconnection import MySQLConnector
-mysql = MySQLConnector(app, 'the_wall_db')
+mysql = MySQLConnector(app, 'friendsdb')
 
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+@app.route('/')
+@app.route('/users')
+def index():
+	query = "SELECT * FROM friends"
+	users = mysql.query_db (query)
+	return render_template('index.html', users=users)
+
+
+
+app.run(debug=True)
